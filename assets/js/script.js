e22814879a30eba66799cce216f3da21b5c595cd
Ceddix <49922771+Ceddix@users.noreply.github.com>
2022-12-09 00:21:28 +0100

    added auto age calculation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,23 @@
+// Calculate Age
+const getAge = (birthDateString) => {
+  const today = new Date();
+  const birthDate = new Date(birthDateString);
+
+  const yearsDifference = today.getFullYear() - birthDate.getFullYear();
+
+  if (
+    today.getMonth() < birthDate.getMonth() ||
+    (today.getMonth() === birthDate.getMonth() && today.getDate() < birthDate.getDate())
+  ) {
+    return yearsDifference - 1;
+  }
+
+  return yearsDifference;
+};
+
+// Typing Effect
 let textLength = 0;
-let text = 'Hi, I\'m Cedrik! I\'m a 17 y/o student from Belgium!';
+let text = 'Hi, I\'m Cedrik! I\'m a ' + getAge('2005-10-22') + ' y/o student from Belgium!';
 
 function type() {
     let textChar = text.charAt(textLength++);
