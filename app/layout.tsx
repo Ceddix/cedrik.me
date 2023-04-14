135c007e061857963f8bc511a3e94d33b6c4bc97
Ceddix <49922771+Ceddix@users.noreply.github.com>
2023-04-15 00:32:05 +0200

    added pages

---
+++ app/layout.tsx
@@ -0,0 +1,54 @@
+import './globals.css'
+import Image from "next/image";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+    subsets: ["latin"],
+    weight: "400",
+    variable: "--font-poppins"
+})
+
+export const metadata = {
+    title: {
+        default: 'Cedrik Secic',
+        template: '%s | Cedrik Secic',
+    },
+    description: 'Cedrik\'s Personal Website',
+    authors: [{ name: "Cedrik Secic" }],
+    icons: {
+        icon: '/favicon.ico',
+    },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+    <body className={`${poppins.variable} font-sans`}>
+
+    <div className={`h-screen`}>
+
+        <Image
+            src={`/profile.png`}
+            alt={`background`}
+            className={`object-cover object-center`}
+            fill={true}
+            quality={100}
+        />
+
+        <div className={`h-screen backdrop-blur-2xl bg-[#16161A]/80 shadow-inner`} style={{ boxShadow: "inset 0 0 20px 1px #141417" }}>
+
+            {children}
+
+        </div>
+
+
+    </div>
+
+    </body>
+    </html>
+  )
+}
