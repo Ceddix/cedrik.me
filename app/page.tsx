c3647b8ea5d12002f20e575178b87d70952f3073
Ceddix <49922771+Ceddix@users.noreply.github.com>
2023-04-20 17:37:07 +0200

    improved age calculation (see "renamed and improved components")

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import Image from "next/image";
-import Typewriter from '@/app/components/typewriter';
-import AgeCalculator from '@/app/components/age-calculator';
+import TypeWriter from '@/app/components/TypeWriter';
+import SocialLinks from "@/app/components/SocialLinks";
 import { Poppins } from "next/font/google";
-import { PageWrapper } from "@/app/components/page-wrapper";
-import {SocialLinks} from "@/app/components/social-links";
+import { PageWrapper } from "@/app/components/PageWrapper";
+import { age } from "@/app/components/Constants"
 
 const poppins = Poppins({
     subsets: ["latin"],
@@ -14,28 +14,35 @@ const poppins = Poppins({
 
 export default function Home() {
   return (
-      <PageWrapper>
 
-          <main className={"flex flex-col justify-center items-center text-center min-h-[70vh]"}>
+      // Navbar (delay with text)
+      // Activity
 
-              <Image
-                  src={`/profile.png`}
-                  alt={`profile`}
-                  className={`inline-block object-cover rounded-full border-[10px] border-[#21222b93] mb-2.5 cursor-pointer transition transform duration-500 hover:scale-105`}
-                  quality={100}
-                  width={170}
-                  height={170}
-              />
+      <>
 
+          <PageWrapper>
 
-              <h1 className={`text-3xl font-bold pb-1`}>Cedrik Secic</h1>
+              <main className={"flex flex-col justify-center items-center text-center min-h-[70vh]"}>
 
-              <Typewriter text={`Hi, I'm Cedrik! I'm a ` + AgeCalculator('2005-10-22') + ` y/o student from Belgium!`} typeSpeed={80} startDelay={500} className={`text-[18px] text-[#94a1b2]`} />
+                  <Image
+                      src={`/profile.png`}
+                      alt={`profile`}
+                      className={`inline-block object-cover rounded-full border-[10px] border-[#21222b93] mb-2.5 cursor-pointer transition transform duration-500 hover:scale-105`}
+                      quality={100}
+                      width={170}
+                      height={170}/>
 
-              <SocialLinks />
+                  <h1 className={`text-3xl font-bold pb-1`}>Cedrik Secic</h1>
 
-          </main>
+                  <TypeWriter text={`Hi, I'm Cedrik! I'm a ${age.toString()} y/o student from Belgium!`} typeSpeed={80}
+                              startDelay={500} className={`text-[18px] text-[#94a1b2]`}/>
 
-      </PageWrapper>
+                  <SocialLinks/>
+
+              </main>
+
+          </PageWrapper>
+
+      </>
   )
 }
