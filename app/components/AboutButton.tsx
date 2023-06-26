11f2a0f5ab6694020b7dc0b3c6b1a22b5e0e05ec
Ceddix <seciccedrik@gmail.com>
2023-06-26 23:58:41 +0200

    feat: improved about button and moved it to components

---
+++ app/components/AboutButton.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import React, {useEffect} from 'react';
+import {stagger, useAnimate} from 'framer-motion';
+import Link from "next/link";
+
+export default function AboutButton() {
+
+    const [scope, animate] = useAnimate()
+
+    useEffect(() => {
+
+        animate("a", {
+            opacity: [0, 1], y: [.5, 0]
+        }, {
+            delay: 6.5
+        })
+
+    })
+
+    return (
+        <div className={"w-full max-w-lg px-9"} ref={scope}>
+            <Link
+                href={"/about"}
+                className={"inline-block group rounded w-full bg-neutral-800/[.4] border border-gray-300/[0.1] mt-2 mb-1 pb-2 pt-2.5 text-sm leading-normal text-neutral-50 shadow-lg transition duration-200 ease-in-out hover:bg-neutral-900/[.6] focus:bg-neutral-900/[.6] focus:outline-none focus:ring-0 active:bg-neutral-950"}>
+                more
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                     className="inline ml-1 transition duration-1000 group-hover:translate-x-[.2rem]" viewBox="0 0 16 16">
+                    <path fillRule="evenodd"
+                          d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
+                </svg>
+            </Link>
+        </div>
+    )
+
+}
--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import SocialLinks from "@/app/components/SocialLinks";
 import { Poppins } from "next/font/google";
 import { PageWrapper } from "@/app/components/PageWrapper";
 import { age } from "@/app/components/Constants"
+import AboutButton from "@/app/components/AboutButton";
 
 const poppins = Poppins({
     subsets: ["latin"],
@@ -42,17 +43,7 @@ export default function Home() {
                         className={`text-[18px] text-[#94a1b2]`}
                     />
 
-                    <a
-                        href={"/about"}
-                        className="inline-block group rounded max-w-sm w-96 min-w-0 bg-neutral-800/[.4] border border-gray-300/[0.1] mt-2 mb-1 mx-8 px-6 pb-2 pt-2.5 text-sm leading-normal text-neutral-50 shadow-lg transition duration-200 ease-in-out hover:bg-neutral-900/[.6] focus:bg-neutral-900/[.6] focus:outline-none focus:ring-0 active:bg-neutral-950">
-                        more
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                             className="inline ml-1 transition duration-1000 group-hover:translate-x-[.2rem]" viewBox="0 0 16 16">
-                            <path fillRule="evenodd"
-                                  d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z"/>
-                        </svg>
-                        {/*<i className="fa-solid fa-arrow-right ml-0.5 transition duration-1000 group-hover:translate-x-0.5"></i>*/}
-                    </a>
+                    <AboutButton/>
 
                     <SocialLinks/>
 
