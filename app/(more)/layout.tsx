84f732b6055f65e4055bee5b9d20e7ffb5ab1cd0
Ceddix <seciccedrik@gmail.com>
2023-06-27 00:04:08 +0200

    feat: added custom layout for about pages

---
+++ app/(more)/layout.tsx
@@ -0,0 +1,26 @@
+import { NavBar } from "@/app/components/NavBar";
+import React from "react";
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+
+  return (
+      <>
+
+          <div className={"flex flex-col h-screen overflow-y-auto items-center"}>
+
+              <NavBar/>
+
+              <main className={"flex-1 p-5 py-20 max-w-3xl w-full"}>
+
+                  <div className={"relative overflow-y-hidden"}>
+
+                      {children}
+
+                  </div>
+
+              </main>
+
+          </div>
+
+      </>
+  )
+}
