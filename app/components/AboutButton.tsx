109c5728296678c28eb2c88267ad83e59474b14c
Ceddix <49922771+Ceddix@users.noreply.github.com>
2024-01-05 02:08:24 +0100

    feat: redesigned about button

--- app/components/AboutButton.tsx
+++ app/components/AboutButton.tsx
@@ -8,11 +8,11 @@ import { TbArrowNarrowRight } from 'react-icons/tb';
 export default function AboutButton() {
 
   return (
-    <div className={"w-full max-w-[31rem] px-9"}>
+    <div className={"w-full max-w-[31.15rem] px-9"}>
       <Link
         href={"/about"}
         className={
-          "group my-3 inline-block w-full rounded border-2 border-gray-300/[0.8] bg-neutral-700/[.4] py-2 text-md leading-normal text-gray-200 shadow-lg transition duration-200 ease-in-out hover:bg-neutral-600/[.6] focus:bg-neutral-900/[.6] focus:outline-none focus:ring-0 active:bg-neutral-950"
+          "group my-3 inline-block w-full rounded border-2 border-gray-300/[0.3] bg-neutral-700/[.4] py-2 text-md leading-normal text-gray-200 shadow-lg transition duration-200 ease-in-out hover:bg-neutral-600/[.6] focus:bg-neutral-900/[.6] focus:outline-none focus:ring-0 active:bg-neutral-950"
         }
       >
         about me
