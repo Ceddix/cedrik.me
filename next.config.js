ab3cb5453e7f9018031532a07949e9204c5f2b06
Ceddix <seciccedrik@gmail.com>
2024-01-21 23:55:20 +0100

    feat: added some redirects

--- next.config.js
+++ next.config.js
@@ -1,6 +1,37 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-
-};
-
-module.exports = nextConfig;
+module.exports = {
+    async redirects() {
+        return [
+            {
+                source: '/github',
+                destination: 'https://github.com/ceddix/',
+                permanent: true,
+            },
+            {
+                source: '/spotify',
+                destination: 'https://open.spotify.com/user/2fy35e57mzi84b2zhafnu7t5r?si=b11fa648285e413f',
+                permanent: true,
+            },
+            {
+                source: '/instagram',
+                destination: 'https://instagram.com/cedrik.sc_',
+                permanent: true,
+            },
+            {
+                source: '/threads',
+                destination: 'https://threads.net/cedrik.sc_',
+                permanent: true,
+            },
+            {
+                source: '/discord',
+                destination: 'https://discord.com/users/463620307245072384',
+                permanent: true,
+            },
+            {
+                source: '/linkedin',
+                destination: 'https://www.linkedin.com/in/cedrik-secic/',
+                permanent: true,
+            },
+        ]
+    },
+}
