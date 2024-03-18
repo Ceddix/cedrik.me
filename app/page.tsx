b15b998092644e14ed68de59c5127b91b10a1962
Ceddix // Cedrik <SecicCedrik@gmail.com>
2024-03-18 16:13:52 +0100

    feat: re-enabled "about button" (WIP)

--- app/page.tsx
+++ app/page.tsx
@@ -41,20 +41,15 @@ export default function Home() {
 
                     <div className={"w-full max-w-[31.15rem] px-9"}>
                         <Link
-                            /*href={"/about"}
+                            href={"/about"}
                             className={
                                 "group my-3 inline-block w-full rounded border-2 border-gray-300/[0.3] bg-neutral-700/[.4] py-2 text-md leading-normal text-gray-200 shadow-lg transition duration-200 ease-in-out hover:bg-neutral-600/[.6] focus:bg-neutral-900/[.6] focus:outline-none focus:ring-0 active:bg-neutral-950"
-                            }*/
-                            href={"/"}
-                            className={
-                                "opacity-60 pointer-events-none group my-3 inline-block w-full rounded border-2 border-gray-300/[0.3] bg-neutral-700/[.4] py-2 text-md leading-normal text-gray-200 shadow-lg"
                             }
                         >
-                            about me (coming soon)
-                            {/*about me
+                            about me
                             <TbArrowNarrowRight
                                 className={`ml-1 inline transition duration-1000 group-hover:translate-x-[.2rem]`}
-                            />*/}
+                            />
                         </Link>
                     </div>
 
