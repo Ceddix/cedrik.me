085d8b965fc17a11897d2414abc26caaeb1167eb
Ceddix // Cedrik <SecicCedrik@gmail.com>
2024-03-18 16:02:16 +0100

    feat: added url shortener (via middleware - no interface yet!)

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,17 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
+import {sql} from '@vercel/postgres';
 
 export const config = {
-    matcher: ['/f(iles)?/(.*)'],
+    matcher: ['/f(iles)?/(.*)','/s(hort)?/(.*)'],
 }
 
 export async function middleware(request: NextRequest) {
     const path = request.nextUrl.pathname;
+    const segments = path.split('/');
 
     // URL rewrite for ShareX uploads
     if (path.startsWith('/f/') || path.startsWith('/files/')) {
-        const segments = path.split('/');
         const newPath = `https://craft-together-mc.de/~ceddix/uploads/${segments.slice(2).join('/')}`;
         const response = await fetch(new URL(newPath, request.url));
 
@@ -19,6 +20,27 @@ export async function middleware(request: NextRequest) {
         }
 
         return response;
+    }
+
+    // URL shortener with Vercel database
+    if (path.startsWith('/s/') || path.startsWith('/short/')) {
+        // Making a SQL query to select a link from the links table where the alias matches the provided slug
+        // The result is limited to 1 row
+        const {rows} =
+            await sql`SELECT * FROM links WHERE alias=${segments.slice(2).join('/')} LIMIT 1`;
+
+        // If no rows are returned, return a response indicating the slug is not in the record
+        if (rows.length === 0) {
+            return NextResponse.rewrite(new URL('/not-found', request.url))
+        }
+
+        // If a row is returned, increment the visit_count for the link with the provided slug
+        if (rows[0]) {
+            await sql`UPDATE links SET visit_count = visit_count + 1 WHERE alias = ${segments.slice(2).join('/')}`;
+        }
+
+        // Redirect to the target of the first row (the selected link)
+        return NextResponse.redirect(rows[0].target, 302);
     }
 
 }
