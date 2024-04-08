8d55c18e3807c5bdd20002c59d4b9536396fc95a
Ceddix // Cedrik <SecicCedrik@gmail.com>
2024-04-08 04:03:26 +0200

    feat: improved about me page
    - some improvements to the contact section
    - some overall improvements
    to the text
    - added buttons for the CraftTogetherMC links (not sure if I
    like this)

--- app/components/ui/ContactLink.tsx
+++ app/components/ui/ContactLink.tsx
@@ -1,15 +1,14 @@
 import React, { ReactElement } from "react";
 import Link from "next/link";
-import {TbExternalLink} from "react-icons/tb";
 
 export const ContactLink = ({name, icon, link}: {name: string; icon: ReactElement; link: string}) => {
 
     return (
             <Link
-                className={`flex flex-row rounded border-2 border-gray-300/[0.3] bg-neutral-700/[.4] p-4 text-md shadow-lg no-underline space-x-2 items-center`}
-                href={link}>
+                className={`flex flex-row flex-1 rounded border-2 border-gray-300/[0.3] bg-neutral-700/[.4] p-4 text-md shadow-lg no-underline space-x-2 items-center hover:bg-neutral-400/[.2]`}
+                href={link} target={"_blank"}>
                 <div className={`text-3xl`}>{icon}</div>
-                <div>{name} <small className={`inline-flex`}><TbExternalLink /></small></div>
+                <div>{name}</div>
             </Link>
     );
 }
